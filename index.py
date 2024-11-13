10b516cc570e33cd8991945057a057e2e537e757
Yuvraj Soni <104073156+YUVI-09@users.noreply.github.com>
2024-11-14 04:14:56 +0530

    Update index.py

--- index.py
+++ index.py
@@ -218,4 +218,4 @@ def health_check():
     return jsonify(status)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=10000)
